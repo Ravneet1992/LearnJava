7318d071a12d59a1a604184ab14c23770c9ae7af
ravneetkaur <ravneetkaur@192.168.1.19>
2019-02-16 21:10:20 -0500

    Grade System

---
+++ src/basicjava/GradePercentage.java
@@ -0,0 +1,26 @@
+package basicjava;
+
+import java.util.Scanner;
+
+public class GradePercentage {
+
+	public static void main(String[] args) {
+		Scanner input  = new Scanner(System.in);
+		System.out.print("What is your overall grade? ");
+		int grade  = input.nextInt();
+
+		if(grade>60) {
+			System.out.println("Admitted");
+		} else if(grade>40 ){
+			System.out.println(" In waiting list");
+		} else {
+			System.out.println("Sorry! Try next time.");
+
+
+
+		}
+
+
+	}
+
+}
