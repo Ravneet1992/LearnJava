9977f182a2f3fa0008c0ee6b437a09131a07ff20
ravneetkaur <ravneetkaur@192.168.1.19>
2019-02-16 22:48:58 -0500

    print no of days in month

---
+++ src/basicjava/NoOfDays.java
@@ -0,0 +1,27 @@
+package basicjava;
+import java.util.Scanner;
+
+public class NoOfDays {
+
+	public static void main(String[] args) {
+		
+		int daysofmonth;
+		int i;
+		
+		for (i=1;i<13;i++) {
+			daysofmonth = daysInMonth(i);
+			System.out.println("month "+ i + " has "+daysofmonth + "days");
+		}
+	}
+	
+	public static int daysInMonth(int month) {
+		
+		if ( (month == 1) || (month == 3) || (month==5) || (month==7) || (month==8) || (month==10) || (month==12) ) {	
+			return 31;
+		} else if( (month==4) || (month==6) || (month==9) || (month==11) ) {
+			return 30;
+		} else {
+			return 28;
+		}
+	}
+}
