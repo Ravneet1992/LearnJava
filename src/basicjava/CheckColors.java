3093ed6e42d8170ce4aa10be8a1c1a7adcb34cee
ravneetkaur <ravneetkaur@192.168.1.19>
2019-02-17 17:23:01 -0500

    Choose color

--- src/basicjava/CheckColors.java
+++ src/basicjava/CheckColors.java
@@ -6,8 +6,20 @@ public class CheckColors {
 	
      public static void main(String[] args) {
     	 Scanner input  = new Scanner(System.in);
-		System.out.print("What color do you want? ");
-		String color = input.next();
+		 System.out.print("What color do you want? ");
+		 String color = input.next();
+		 if((color.equals("R")) || (color.equals("r"))) {
+			System.out.println("You have chosen Red.");
+		} else if((color.equals("B")) || (color.equals("b"))) {
+			System.out.println("You have chosen Blue.");
+		} else if((color.equals("G")) || (color.equals("g"))) {
+			System.out.println("You have chosen Green.");
+		} else {
+			System.out.println("Unknown color: " + color);
+		}
+		
+		
+		input.close();
 	}
 
 }
