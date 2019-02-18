318afe76033175dd14ac9418b7e389ad036fbc18
ravneetkaur <ravneetkaur@192.168.1.19>
2019-02-18 12:15:05 -0500

    Another java program

---
+++ src/basicjava/TickTackToe.java
@@ -0,0 +1,15 @@
+package basicjava;
+
+public class TickTackToe {
+
+	public static void main(String[] args) {
+		
+		for(int i =0; i<=2; i++) {
+			System.out.println("+---+---+---+");
+			System.out.println("|   |   |   |");
+		}
+	     System.out.println("+---+---+---+");
+
+	}
+
+}
