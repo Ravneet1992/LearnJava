e019d515ded8f366c7769dddf558764e432cce63
ravneetkaur <ravneetkaur@192.168.1.30>
2019-03-17 11:34:06 -0400

    convert integer to String

---
+++ src/basicjava/ConvertInteger.java
@@ -0,0 +1,12 @@
+package basicjava;
+
+public class ConvertInteger {
+
+	public static void main(String[] args) {
+		int num = 6;
+		String testText = "" + num;
+		System.out.println(testText + 5);
+
+	}
+
+}
