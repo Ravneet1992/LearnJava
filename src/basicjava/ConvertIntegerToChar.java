5ddc2884b2f9b2a8d89bb99ae2dab1224f0170e9
ravneetkaur <ravneetkaur@192.168.1.30>
2019-03-17 16:28:27 -0400

    convert integer to string

---
+++ src/basicjava/ConvertIntegerToChar.java
@@ -0,0 +1,12 @@
+package basicjava;
+
+public class ConvertIntegerToChar {
+
+	public static void main(String[] args) {
+		int num = 554;
+		char ch  = ("" + num).charAt(2);
+		System.out.println(ch);
+
+	}
+
+}
