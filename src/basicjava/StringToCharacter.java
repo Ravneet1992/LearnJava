92d38eb6d487c7bd9e95dfc9282628571503190a
ravneetkaur <ravneetkaur@192.168.1.30>
2019-03-17 16:21:54 -0400

    convert string to character

---
+++ src/basicjava/StringToCharacter.java
@@ -0,0 +1,12 @@
+package basicjava;
+
+public class StringToCharacter {
+
+	public static void main(String[] args) {
+		String word = "Hello";
+		char ch = word.charAt(4);
+		System.out.println(ch);
+
+	}
+
+}
