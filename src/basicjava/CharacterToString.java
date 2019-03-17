7c89c5b3aa3a6e1159a9c225bcf333e69faf251e
ravneetkaur <ravneetkaur@192.168.1.30>
2019-03-17 11:44:53 -0400

    convert character to string

---
+++ src/basicjava/CharacterToString.java
@@ -0,0 +1,12 @@
+package basicjava;
+
+public class CharacterToString {
+
+	public static void main(String[] args) {
+		char ch = '5';
+		String test = "" + ch;
+		System.out.println(test + 5);;
+
+	}
+
+}
