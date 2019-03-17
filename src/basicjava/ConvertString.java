5bfb18b463157c649ff1b00fb80d361cec6f4a94
ravneetkaur <ravneetkaur@192.168.1.30>
2019-03-17 11:23:30 -0400

    convert string into an integer

---
+++ src/basicjava/ConvertString.java
@@ -0,0 +1,13 @@
+package basicjava;
+
+public class ConvertString {
+
+	public static void main(String[] args) {
+		
+		String testText = "5";
+		int num = Integer.parseInt(testText);
+		System.out.println(num + 5);
+
+	}
+
+}
