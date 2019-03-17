7c19c27b4cb9cf280877e4bf2e53f9db8209d67b
ravneetkaur <ravneetkaur@192.168.1.30>
2019-03-17 11:54:46 -0400

    convert character to integer

---
+++ src/basicjava/CharacterToInteger.java
@@ -0,0 +1,12 @@
+package basicjava;
+
+public class CharacterToInteger {
+
+	public static void main(String[] args) {
+		char ch = '5';
+		int num = Integer.parseInt("" + ch);
+		System.out.println(num + 5);	
+
+	}
+
+}
